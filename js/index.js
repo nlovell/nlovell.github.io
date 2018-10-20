0e551dbe9679a32e2f44994b853d32b348e77bb6
nlovell-net <43500865+nlovell-net@users.noreply.github.com>
2018-10-20 21:02:58 +0100

    conceptual overhaul
    yes

---
+++ js/index.js
@@ -0,0 +1,153 @@
+(function() {
+	"use strict";
+
+	var canvas = document.querySelector("#tv"),
+		context = canvas.getContext("gl") || canvas.getContext("2d"),
+		scaleFactor = 2.5, // Noise size
+		samples = [],
+		sampleIndex = 0,
+		scanOffsetY = 0,
+		scanSize = 0,
+		FPS = 50,
+		scanSpeed = FPS * 15, // 15 seconds from top to bottom
+		SAMPLE_COUNT = 10;
+
+	window.onresize = function() {
+		canvas.width = canvas.offsetWidth / scaleFactor;
+		canvas.height = canvas.width / (canvas.offsetWidth / canvas.offsetHeight);
+		scanSize = (canvas.offsetHeight / scaleFactor) / 3;
+
+		samples = []
+		for(var i = 0; i < SAMPLE_COUNT; i++)
+			samples.push(generateRandomSample(context, canvas.width, canvas.height));
+	};
+
+	function interpolate(x, x0, y0, x1, y1) {
+		return y0 + (y1 - y0)*((x - x0)/(x1 - x0));
+	}
+
+
+	function generateRandomSample(context, w, h) {	
+		var intensity = [];
+		var random = 0;
+		var factor = h / 50;
+		var trans = 1 - Math.random() * 0.05;
+
+		var intensityCurve = [];
+		for(var i = 0; i < Math.floor(h / factor) + factor; i++)
+			intensityCurve.push(Math.floor(Math.random() * 15));
+
+		for(var i = 0; i < h; i++) {
+			var value = interpolate((i/factor), Math.floor(i / factor), intensityCurve[Math.floor(i / factor)], Math.floor(i / factor) + 1, intensityCurve[Math.floor(i / factor) + 1]);
+			intensity.push(value);
+		}
+
+		var imageData = context.createImageData(w, h);
+		for(var i = 0; i < (w * h); i++) {
+			var k = i * 4;
+			var color = Math.floor(36 * Math.random());
+			// Optional: add an intensity curve to try to simulate scan lines
+			color += intensity[Math.floor(i / w)];
+			imageData.data[k] = imageData.data[k + 1] = imageData.data[k + 2] = color;
+			imageData.data[k + 3] = Math.round(255 * trans);
+		}
+		return imageData;
+	} 
+
+	function render() {
+		context.putImageData(samples[Math.floor(sampleIndex)], 0, 0);
+
+		sampleIndex += 20 / FPS; // 1/FPS == 1 second
+		if(sampleIndex >= samples.length) sampleIndex = 0;
+
+		var grd = context.createLinearGradient(0, scanOffsetY, 0, scanSize + scanOffsetY);
+
+		grd.addColorStop(0, 'rgba(255,255,255,0)');
+		grd.addColorStop(0.1, 'rgba(255,255,255,0)');
+		grd.addColorStop(0.2, 'rgba(255,255,255,0.2)');
+		grd.addColorStop(0.3, 'rgba(255,255,255,0.0)');
+		grd.addColorStop(0.45, 'rgba(255,255,255,0.1)');
+		grd.addColorStop(0.5, 'rgba(255,255,255,1.0)');
+		grd.addColorStop(0.55, 'rgba(255,255,255,0.55)');
+		grd.addColorStop(0.6, 'rgba(255,255,255,0.25)');
+		//grd.addColorStop(0.8, 'rgba(255,255,255,0.15)');
+		grd.addColorStop(1, 'rgba(255,255,255,0)');
+
+		context.fillStyle = grd;
+		context.fillRect(0, scanOffsetY, canvas.width, scanSize + scanOffsetY);
+		context.globalCompositeOperation = "lighter";
+
+		scanOffsetY += (canvas.height / scanSpeed);
+		if(scanOffsetY > canvas.height) scanOffsetY = -(scanSize / 2);
+
+		window.requestAnimationFrame(render);
+	}
+	window.onresize();
+	window.requestAnimationFrame(render);
+})();
+
+
+
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+
+	/* this one doesn't work in a linear fashion */
+var element = document.getElementById("element");
+
+TweenMax.to(element, 5, {x:"+=30", yoyo:true, repeat:-1});
+TweenMax.to(element, 5, {x:"-=30", yoyo:true, repeat:-1});
